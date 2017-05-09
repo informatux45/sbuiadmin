2e13ed441b1c9d9c6dea508c3fa471e68178bec2
BooBoo <informatux45@gmail.com>
2017-05-09 14:08:59 +0200

    New function Browser

--- inc/functions.php
+++ inc/functions.php
@@ -865,4 +865,21 @@ function insert_sbGetSeoMetas() {
 	return $metas_seo;
 }
 
+/**
+ * Get Browser
+ * @param string Browser name
+ * @return String
+ */
+function insert_sbGetBrowser($param) {
+	$user_agent = $param['ua'];
+    if (strpos($user_agent, 'Opera') || strpos($user_agent, 'OPR/')) return 'Opera';
+    elseif (strpos($user_agent, 'Edge')) return 'Edge';
+    elseif (strpos($user_agent, 'Chrome')) return 'Chrome';
+    elseif (strpos($user_agent, 'Safari')) return 'Safari';
+    elseif (strpos($user_agent, 'Firefox')) return 'Firefox';
+    elseif (strpos($user_agent, 'MSIE') || strpos($user_agent, 'Trident/7')) return 'IE';
+   
+    return 'Other';
+}
+
 ?>
