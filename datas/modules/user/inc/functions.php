77d217313b0a4d381c4193394b6d9b9cc01cb4a6
BooBoo <informatux45@gmail.com>
2017-05-18 16:41:01 +0200

    User module shortcode modified

--- datas/modules/user/inc/functions.php
+++ datas/modules/user/inc/functions.php
@@ -17,6 +17,8 @@ if (!defined('SB_PATH')) {
  * Get A TABBS with ID
  * id		int			$param id
  * name		string		$param name (function name after 'shortcode_')
+ * Editeur : [CS name=sbuser icontext=1]
+ * Smarty : {insert name="sbDoShortcode" code="[CS name=sbuser icontext=1]"}
  * return HTML
  */
 function shortcode_sbuser($param = '') {
@@ -24,24 +26,32 @@ function shortcode_sbuser($param = '') {
 
 	// --- Initialization
 	$icon_text = ($param['icontext'] == '1') ? true : false;
+	$icon_menu = ($param['menu'] == 'li') ? true : false;
 	// --- Initialization URL
 	$login_url  = (SBREWRITEURL === true) ? SB_URL . 'user' : SB_URL . 'index.php?p=user';
 	$logout_url = (SBREWRITEURL === true) ? SB_URL . 'user/?ac=logout' : SB_URL . 'index.php?p=user&ac=logout';
 	$login_text = (isset($_SESSION['sbmagic_user_name']) && $_SESSION['sbmagic_user_name'] != '') ? 'Profil' : 'Login';
 	// --- Initialization LOGIN / PROFILE
 	$user_html  = '';
+	if ($icon_menu) $user_html .= '<li>';
 	$user_html .= '<a href="' . $login_url . '">';
 	$user_html .= '<i class="fa fa-user"></i>';
 	if ($icon_text) $user_html .= ' ' . $login_text;
 	$user_html .= '</a>';
+	if ($icon_menu) $user_html .= '</li>';
+
 	// --- Check if user is logging
 	if (isset($_SESSION['sbmagic_user_name']) && $_SESSION['sbmagic_user_name'] != '') {
 		// --- Initialization LOGOUT
-		$user_html .= '&nbsp;&nbsp;';
+		if ($icon_menu)
+			$user_html .= '<li>';
+		else
+			$user_html .= '&nbsp;&nbsp;';
 		$user_html .= '<a href="' . $logout_url . '">';
 		$user_html .= '<i class="fa fa-lock"></i>';
 		if ($icon_text) $user_html .= ' logout';
 		$user_html .= '</a>';		
+		if ($icon_menu) $user_html .= '</li>';
 	}
 
 	return $user_html;
