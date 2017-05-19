44b93c0f10b6fb8856f3c01b540baebe9b564170
BooBoo <informatux45@gmail.com>
2017-05-19 11:24:20 +0200

    Add option shortcode module USER

--- datas/modules/user/inc/functions.php
+++ datas/modules/user/inc/functions.php
@@ -14,27 +14,33 @@ if (!defined('SB_PATH')) {
 }
 
 /**
- * Get A TABBS with ID
- * id		int			$param id
- * name		string		$param name (function name after 'shortcode_')
- * Editeur : [CS name=sbuser icontext=1]
- * Smarty : {insert name="sbDoShortcode" code="[CS name=sbuser icontext=1]"}
+ * Get USER MENU
+ * icontext		int			$param icontext
+ * name			string		$param name (function name after 'shortcode_')
+ * menu 		string		$param menu (li)
+ * menu_class	string		$param menu_class (class for li tag)
+ * href_class	string		$param href_class (class for a tag)
+ * Editeur WYSIWYG : [CS name=sbuser icontext=1 menu_class=mymenuclass href_class=myhrefclass]
+ * Smarty TPLs : {insert name="sbDoShortcode" code="[CS name=sbuser icontext=1 menu_class=mymenuclass href_class=myhrefclass]"}
  * return HTML
  */
 function shortcode_sbuser($param = '') {
 	global $sbsanitize;
 
 	// --- Initialization
-	$icon_text = ($param['icontext'] == '1') ? true : false;
-	$icon_menu = ($param['menu'] == 'li') ? true : false;
+	$icon_text = $icon_menu = $class_menu = $class_href = $user_html = "";
+	// --- Assign variables
+	$icon_text  = ($param['icontext'] == '1') ? true : false;
+	$icon_menu  = ($param['menu'] == 'li') ? true : false;
+	$class_menu = (isset($param['menu_class'])) ? $param['menu_class'] : "";
+	$class_href = (isset($param['href_class'])) ? $param['href_class'] : "";
 	// --- Initialization URL
 	$login_url  = (SBREWRITEURL === true) ? SB_URL . 'user' : SB_URL . 'index.php?p=user';
 	$logout_url = (SBREWRITEURL === true) ? SB_URL . 'user/?ac=logout' : SB_URL . 'index.php?p=user&ac=logout';
 	$login_text = (isset($_SESSION['sbmagic_user_name']) && $_SESSION['sbmagic_user_name'] != '') ? 'Profil' : 'Login';
 	// --- Initialization LOGIN / PROFILE
-	$user_html  = '';
-	if ($icon_menu) $user_html .= '<li>';
-	$user_html .= '<a href="' . $login_url . '">';
+	if ($icon_menu) $user_html .= '<li class="' . $class_menu . '">';
+	$user_html .= '<a href="' . $login_url . '" class="' . $class_href . '">';
 	$user_html .= '<i class="fa fa-user"></i>';
 	if ($icon_text) $user_html .= ' ' . $login_text;
 	$user_html .= '</a>';
