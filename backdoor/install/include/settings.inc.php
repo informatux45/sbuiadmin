bc488f25cdf2a1e4062ff9f49594e68828996688
informatux45 <informatux45@gmail.com>
2019-04-01 22:57:58 +0200

    Change version of installer

--- backdoor/install/include/settings.inc.php
+++ backdoor/install/include/settings.inc.php
@@ -7,7 +7,7 @@
     define('EI_MODE', 'production');
     
     // *** version number of SBUIADMIN Installer
-    define('EI_VERSION', '2.0.2');
+    define('EI_VERSION', '2.1.0');
 
     // *** default language: en - English
     define('EI_DEFAULT_LANGUAGE', 'fr');
