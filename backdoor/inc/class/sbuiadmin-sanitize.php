64c962e358ef7384caec2ebde293c8a3f6664693
informatux45 <informatux45@users.noreply.github.com>
2020-03-02 12:13:55 +0100

    Change function

--- backdoor/inc/class/sbuiadmin-sanitize.php
+++ backdoor/inc/class/sbuiadmin-sanitize.php
@@ -86,7 +86,7 @@ class sanitize extends sql {
 	function stripslashes_deep($value) {
 		$value = is_array($value) ?
 					array_map('stripslashes_deep', $value) :
-					stripslashes($value);
+					$this->stripSlashesGPC($value);
 	
 		return $value;
 	}
