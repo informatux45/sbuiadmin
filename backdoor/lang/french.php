7753c95a1d98dae423b80f67273805a000757020
informatux45 <informatux@e.email>
2024-07-16 19:20:26 +0200

    Update menu fonctionnalities

--- backdoor/lang/french.php
+++ backdoor/lang/french.php
@@ -11,24 +11,54 @@
  */
 
 // Log message
-define("SBUIADMIN_MSG_LOG_ACCESS_GRANTED",			"Utilisateur [%s] identifi&eacute; depuis [%s]");
-define("SBUIADMIN_MSG_LOG_ACCESS_CAPTCHA_ERROR",		"Utilisateur [%s] identifi&eacute; depuis [%s] avec une erreur captcha");
-define("SBUIADMIN_MSG_LOG_ACCESS_CAPTCHA_ERROR2",		"Utilisateur [%s] identifi&eacute; depuis [%s] avec une erreur captcha [%s]->[%s]");
-define("SBUIADMIN_MSG_LOG_ACCESS_USER_ERROR",			"Utilisateur d&eacute;sactiv&eacute; [%s] a essay&eacute; de s&#39;identifier depuis [%s]");
-define("SBUIADMIN_MSG_LOG_ACCESS_NOGRANTED",			"Identification non autoris&eacute;e depuis [%s] - propablement un intru");
-define("SBUIADMIN_MSG_LOG_ACCESS_MISSING",			"Erreur d&#39;acc&egrave;s depuis [%s] - identifiant et&#47;ou mot de passe manquant");
+defined('SBUIADMIN_MSG_LOG_ACCESS_GRANTED') OR define("SBUIADMIN_MSG_LOG_ACCESS_GRANTED",	"Utilisateur [%s] identifi&eacute; depuis [%s]");
+define("SBUIADMIN_MSG_LOG_ACCESS_CAPTCHA_ERROR",	"Utilisateur [%s] identifi&eacute; depuis [%s] avec une erreur captcha");
+define("SBUIADMIN_MSG_LOG_ACCESS_CAPTCHA_ERROR2",	"Utilisateur [%s] identifi&eacute; depuis [%s] avec une erreur captcha [%s]->[%s]");
+define("SBUIADMIN_MSG_LOG_ACCESS_USER_ERROR",	"Utilisateur d&eacute;sactiv&eacute; [%s] a essay&eacute; de s&#39;identifier depuis [%s]");
+define("SBUIADMIN_MSG_LOG_ACCESS_NOGRANTED",	"Identification non autoris&eacute;e depuis [%s] - propablement un intru");
+define("SBUIADMIN_MSG_LOG_ACCESS_MISSING",	"Erreur d&#39;acc&egrave;s depuis [%s] - identifiant et&#47;ou mot de passe manquant");
  
  // Control access message
-define("SBUIADMIN_MSG_ERROR_E1",						"Captcha incorrect");
-define("SBUIADMIN_MSG_ERROR_E2",						"Login incorrect");
-define("SBUIADMIN_MSG_ERROR_E3",						"Login manquant");
-define("SBUIADMIN_MSG_ERROR_E4",						"Compte d&eacute;sactiv&eacute;");
+define("SBUIADMIN_MSG_ERROR_E1",	"Captcha incorrect");
+define("SBUIADMIN_MSG_ERROR_E2",	"Login incorrect");
+define("SBUIADMIN_MSG_ERROR_E3",	"Login manquant");
+define("SBUIADMIN_MSG_ERROR_E4",	"Compte d&eacute;sactiv&eacute;");
 
 // User Interface
-define("SBUIADMIN_GLOBAL_LAST_LOGIN",					"Derni&egrave;re connexion");
-define("SBUIADMIN_GLOBAL_SETTINGS",					"Param&egrave;tres");
-define("SBUIADMIN_GLOBAL_LOGOUT",						"D&eacute;connexion");
-define("SBUIADMIN_GLOBAL_HI",							"Bonjour");
+define("SBUIADMIN_GLOBAL_LAST_LOGIN",	"Derni&egrave;re connexion");
+define("SBUIADMIN_GLOBAL_SETTINGS",	"Param&egrave;tres");
+define("SBUIADMIN_GLOBAL_LOGOUT",	"D&eacute;connexion");
+define("SBUIADMIN_GLOBAL_HI",	"Bonjour");
  
- 
- ?>
+// Menu (Main)
+// ** Configuration
+defined('SBUIADMIN_MENU_CONFIGURATION') OR define("SBUIADMIN_MENU_CONFIGURATION",	"Configuration");
+defined('SBUIADMIN_MENU_CONFIGURATION_GENERAL') OR define("SBUIADMIN_MENU_CONFIGURATION_GENERAL",	"Générale");
+defined('SBUIADMIN_MENU_CONFIGURATION_CACHE') OR define("SBUIADMIN_MENU_CONFIGURATION_CACHE",	"Cache");
+defined('SBUIADMIN_MENU_CONFIGURATION_DASHBOARD') OR define("SBUIADMIN_MENU_CONFIGURATION_DASHBOARD",	"Dashboard");
+defined('SBUIADMIN_MENU_CONFIGURATION_TOOLBAR') OR define("SBUIADMIN_MENU_CONFIGURATION_TOOLBAR",	"Toolbar CKEditor");
+defined('SBUIADMIN_MENU_CONFIGURATION_THEME') OR define("SBUIADMIN_MENU_CONFIGURATION_THEME",	"Thème");
+defined('SBUIADMIN_MENU_CONFIGURATION_THEME_INFOS') OR define("SBUIADMIN_MENU_CONFIGURATION_THEME_INFOS",	"Thème infos");
+defined('SBUIADMIN_MENU_CONFIGURATION_BOOTSTRAP') OR define("SBUIADMIN_MENU_CONFIGURATION_BOOTSTRAP",	"Bootstrap Samples");
+// ** Logaccess
+defined('SBUIADMIN_MENU_LOG') OR define("SBUIADMIN_MENU_LOG",	"Journaux");
+// ** Users
+defined('SBUIADMIN_MENU_USERS') OR define("SBUIADMIN_MENU_USERS",	"Utilisateurs");
+defined('SBUIADMIN_MENU_ALL_USERS') OR define("SBUIADMIN_MENU_ALL_USERS",	"Tous les utilisateurs");
+defined('SBUIADMIN_MENU_BLOCKED_IPS') OR define("SBUIADMIN_MENU_BLOCKED_IPS",	"IPs Bloquées");
+defined('SBUIADMIN_MENU_GRAVATAR') OR define("SBUIADMIN_MENU_GRAVATAR",	"Gravatar");
+// ** Medias
+defined('SBUIADMIN_MENU_MEDIAS') OR define("SBUIADMIN_MENU_MEDIAS",	"Médias");
+// ** CMS Config
+defined('SBUIADMIN_MENU_CMSCONFIG') OR define("SBUIADMIN_MENU_CMSCONFIG",	"CMS config");
+defined('SBUIADMIN_MENU_CMSCONFIG_HEADERFOOTER') OR define("SBUIADMIN_MENU_CMSCONFIG_HEADERFOOTER",	"Header / Footer");
+defined('SBUIADMIN_MENU_CMSCONFIG_CSS') OR define("SBUIADMIN_MENU_CMSCONFIG_CSS",	"CSS");
+defined('SBUIADMIN_MENU_CMSCONFIG_JAVASCRIPT') OR define("SBUIADMIN_MENU_CMSCONFIG_JAVASCRIPT",	"JAVASCRIPT");
+defined('SBUIADMIN_MENU_CMSCONFIG_COMINGSOON') OR define("SBUIADMIN_MENU_CMSCONFIG_COMINGSOON",	"Coming Soon");
+defined('SBUIADMIN_MENU_CMSCONFIG_MULTILANG') OR define("SBUIADMIN_MENU_CMSCONFIG_MULTILANG",	"Multilangue");
+defined('SBUIADMIN_MENU_CMSCONFIG_PLUGINS') OR define("SBUIADMIN_MENU_CMSCONFIG_PLUGINS",	"Plugins");
+defined('SBUIADMIN_MENU_CMSCONFIG_FONTS') OR define("SBUIADMIN_MENU_CMSCONFIG_FONTS",	"Polices");
+defined('SBUIADMIN_MENU_CMSCONFIG_SEO') OR define("SBUIADMIN_MENU_CMSCONFIG_SEO",	"SEO");
+
+
+?>
--- backdoor/main.php
+++ backdoor/main.php
@@ -22,65 +22,67 @@ defined('SBUIADMIN_PATH') or die('Are you crazy!');
 // ----------------------------------------------------
 // ----------------------------------------------------
 // ----------------------------------------------------
-$module_menu['settings']['main']  = "Configuration";
+$module_menu['settings']['main']  = SBUIADMIN_MENU_CONFIGURATION;
 $module_menu['settings']['icon']  = "cubes";
 $module_menu['settings']['group'] = "admin"; // user OR admin
-//$module_menu['settings']['li'][0]['title'] = "Générale";
-//$module_menu['settings']['li'][0]['link']  = "index.php?p=settings";
-//$module_menu['settings']['li'][1]['title'] = "Cache";
-//$module_menu['settings']['li'][1]['link']  = "index.php?p=cache";
-//$module_menu['settings']['li'][2]['title'] = "Dashboard";
-//$module_menu['settings']['li'][2]['link']  = "index.php?p=dashboard";
-//$module_menu['settings']['li'][3]['title'] = "Toolbar CKEditor";
-//$module_menu['settings']['li'][3]['link']  = "index.php?p=toolbarck";
+$module_menu['settings']['li'][0]['title'] = SBUIADMIN_MENU_CONFIGURATION_GENERAL;
+$module_menu['settings']['li'][0]['link']  = "index.php?p=settings";
+$module_menu['settings']['li'][1]['title'] = SBUIADMIN_MENU_CONFIGURATION_CACHE;
+$module_menu['settings']['li'][1]['link']  = "index.php?p=cache";
+$module_menu['settings']['li'][2]['title'] = SBUIADMIN_MENU_CONFIGURATION_DASHBOARD;
+$module_menu['settings']['li'][2]['link']  = "index.php?p=dashboard";
+$module_menu['settings']['li'][3]['title'] = SBUIADMIN_MENU_CONFIGURATION_TOOLBAR;
+$module_menu['settings']['li'][3]['link']  = "index.php?p=toolbarck";
+$module_menu['settings']['li'][4]['title'] = SBUIADMIN_MENU_CONFIGURATION_THEME;
+$module_menu['settings']['li'][4]['link']  = "index.php?p=theme";
+$module_menu['settings']['li'][5]['title'] = SBUIADMIN_MENU_CONFIGURATION_THEME_INFOS;
+$module_menu['settings']['li'][5]['link']  = "index.php?p=themeinfos";
+$module_menu['settings']['li'][6]['title'] = SBUIADMIN_MENU_CONFIGURATION_BOOTSTRAP;
+$module_menu['settings']['li'][6]['link']  = "assets/samples/";
+$module_menu['settings']['li'][6]['target'] = "_blank";
 
-$module_menu['logaccess']['main']  = "Journaux";
+$module_menu['logaccess']['main']  = SBUIADMIN_MENU_LOG;
 $module_menu['logaccess']['icon']  = "list-alt";
 $module_menu['logaccess']['group'] = "admin"; // user OR admin
-
-$module_menu['database']['main']  = "Database";
-$module_menu['database']['icon']  = "database";
-$module_menu['database']['group'] = "admin"; // user OR admin
-
-$module_menu['explorer']['main']  = "Explorer";
-$module_menu['explorer']['icon']  = "sitemap";
-$module_menu['explorer']['group'] = "admin"; // user OR admin
 
 $module_menu['users']['main']  = "Utilisateurs";
 $module_menu['users']['icon']  = "users";
 $module_menu['users']['group'] = "admin"; // user OR admin
+$module_menu['users']['li'][0]['title']  = SBUIADMIN_MENU_ALL_USERS;
+$module_menu['users']['li'][0]['link']   = "index.php?p=users";
+$module_menu['users']['li'][1]['title']  = SBUIADMIN_MENU_BLOCKED_IPS;
+$module_menu['users']['li'][1]['link']   = "index.php?p=users&a=blockedip";
+$module_menu['users']['li'][2]['title']  = SBUIADMIN_MENU_GRAVATAR;
+$module_menu['users']['li'][2]['link']   = "https://fr.gravatar.com/";
+$module_menu['users']['li'][2]['target'] = "_blank";
 
-$module_menu['medias']['main'] = "Medias";
+$module_menu['medias']['main'] = SBUIADMIN_MENU_MEDIAS;
 $module_menu['medias']['icon'] = "desktop";
 $module_menu['medias']['group'] = "user"; // user OR admin
 
-$module_menu['cmsconfig']['main']  = "CMS config";
+$module_menu['cmsconfig']['main']  = SBUIADMIN_MENU_CMSCONFIG;
 $module_menu['cmsconfig']['icon']  = "gears";
 $module_menu['cmsconfig']['group'] = "user"; // user OR admin
-//$module_menu['cmsconfig']['li'][0]['title'] = "Header / Footer";
-//$module_menu['cmsconfig']['li'][0]['link']  = "index.php?p=cmsconfig&op=headerfooter";
-//$module_menu['cmsconfig']['li'][1]['title'] = "CSS Code";
-//$module_menu['cmsconfig']['li'][1]['link']  = "index.php?p=cmsconfig&op=css";
-//$module_menu['cmsconfig']['li'][2]['title'] = "JAVASCRIPT Code";
-//$module_menu['cmsconfig']['li'][2]['link']  = "index.php?p=cmsconfig&op=javascript";
-//$module_menu['cmsconfig']['li'][3]['title'] = "Coming Soon";
-//$module_menu['cmsconfig']['li'][3]['link']  = "index.php?p=cmsconfig&op=comingsoon";
-//$module_menu['cmsconfig']['li'][4]['title'] = "Multilangue";
-//$module_menu['cmsconfig']['li'][4]['link']  = "index.php?p=cmsconfig&op=multilang";
-//$module_menu['cmsconfig']['li'][5]['title'] = "Plugins";
-//$module_menu['cmsconfig']['li'][5]['link']  = "index.php?p=cmsconfig&op=plugins";
-//$module_menu['cmsconfig']['li'][6]['title'] = "Fonts";
-//$module_menu['cmsconfig']['li'][6]['link']  = "index.php?p=cmsconfig&op=fonts";
-//$module_menu['cmsconfig']['li'][7]['title'] = "SEO";
-//$module_menu['cmsconfig']['li'][7]['link']  = "index.php?p=cmsconfig&op=seo";
+$module_menu['cmsconfig']['li'][0]['title'] = SBUIADMIN_MENU_CMSCONFIG_HEADERFOOTER;
+$module_menu['cmsconfig']['li'][0]['link']  = "index.php?p=cmsconfig&op=headerfooter";
+$module_menu['cmsconfig']['li'][1]['title'] = SBUIADMIN_MENU_CMSCONFIG_CSS;
+$module_menu['cmsconfig']['li'][1]['link']  = "index.php?p=cmsconfig&op=css";
+$module_menu['cmsconfig']['li'][2]['title'] = SBUIADMIN_MENU_CMSCONFIG_JAVASCRIPT;
+$module_menu['cmsconfig']['li'][2]['link']  = "index.php?p=cmsconfig&op=javascript";
+$module_menu['cmsconfig']['li'][3]['title'] = SBUIADMIN_MENU_CMSCONFIG_COMINGSOON;
+$module_menu['cmsconfig']['li'][3]['link']  = "index.php?p=cmsconfig&op=comingsoon";
+$module_menu['cmsconfig']['li'][4]['title'] = SBUIADMIN_MENU_CMSCONFIG_MULTILANG;
+$module_menu['cmsconfig']['li'][4]['link']  = "index.php?p=cmsconfig&op=multilang";
+$module_menu['cmsconfig']['li'][5]['title'] = SBUIADMIN_MENU_CMSCONFIG_PLUGINS;
+$module_menu['cmsconfig']['li'][5]['link']  = "index.php?p=cmsconfig&op=plugins";
+$module_menu['cmsconfig']['li'][6]['title'] = SBUIADMIN_MENU_CMSCONFIG_FONTS;
+$module_menu['cmsconfig']['li'][6]['link']  = "index.php?p=cmsconfig&op=fonts";
+$module_menu['cmsconfig']['li'][7]['title'] = SBUIADMIN_MENU_CMSCONFIG_SEO;
+$module_menu['cmsconfig']['li'][7]['link']  = "index.php?p=cmsconfig&op=seo";
 
 $module_menu['menu']['main']  = "Menu";
 $module_menu['menu']['icon']  = "th-list";
 $module_menu['menu']['group'] = "user"; // user OR admin
-
-$module_menu['blocs']['main']  = "Blocs";
-$module_menu['blocs']['icon']  = "codepen";
-$module_menu['blocs']['group'] = "user"; // user OR admin
 
 // -----------------------------------------------------
 // -----------------------------------------------------
@@ -96,12 +98,12 @@ $module_menu['blocs']['group'] = "user"; // user OR admin
 $module_menu['news']['main']  = "Actualités";
 $module_menu['news']['icon']  = "rss";
 $module_menu['news']['group'] = "user"; // user OR admin
-//$module_menu['news']['li'][0]['title'] = "Tous les articles";
-//$module_menu['news']['li'][0]['link']  = "index.php?p=news";
-//$module_menu['news']['li'][1]['title'] = "Toutes les catégories";
-//$module_menu['news']['li'][1]['link']  = "index.php?p=news&a=category";
-//$module_menu['news']['li'][2]['title'] = "Paramètres";
-//$module_menu['news']['li'][2]['link']  = "index.php?p=news&a=settings";
+$module_menu['news']['li'][0]['title'] = "Tous les articles";
+$module_menu['news']['li'][0]['link']  = "index.php?p=news";
+$module_menu['news']['li'][1]['title'] = "Toutes les catégories";
+$module_menu['news']['li'][1]['link']  = "index.php?p=news&a=category";
+$module_menu['news']['li'][2]['title'] = "Paramètres";
+$module_menu['news']['li'][2]['link']  = "index.php?p=news&a=settings";
 
 // -----------------------
 // Menu PAGES
@@ -109,6 +111,10 @@ $module_menu['news']['group'] = "user"; // user OR admin
 $module_menu['pages']['main']  = "Pages";
 $module_menu['pages']['icon']  = "copy";
 $module_menu['pages']['group'] = "user"; // user OR admin
+$module_menu['pages']['li'][0]['title'] = "Toutes les pages";
+$module_menu['pages']['li'][0]['link']  = "index.php?p=pages";
+$module_menu['pages']['li'][1]['title'] = "Tous les blocs";
+$module_menu['pages']['li'][1]['link']  = "index.php?p=blocs";
 
 // -----------------------
 // Menu CONTACT (Forms)
@@ -116,10 +122,10 @@ $module_menu['pages']['group'] = "user"; // user OR admin
 $module_menu['contact']['main']  = "Contact";
 $module_menu['contact']['icon']  = "envelope";
 $module_menu['contact']['group'] = "user"; // user OR admin
-//$module_menu['contact']['li'][0]['title'] = "Tous les formulaires";
-//$module_menu['contact']['li'][0]['link']  = "index.php?p=contact";
-//$module_menu['contact']['li'][1]['title'] = "Paramètres";
-//$module_menu['contact']['li'][1]['link']  = "index.php?p=contact&a=settings";
+$module_menu['contact']['li'][0]['title'] = "Tous les formulaires";
+$module_menu['contact']['li'][0]['link']  = "index.php?p=contact";
+$module_menu['contact']['li'][1]['title'] = "Paramètres";
+$module_menu['contact']['li'][1]['link']  = "index.php?p=contact&a=settings";
 
 // -----------------------
 // Menu SLIDER
@@ -134,15 +140,15 @@ $module_menu['slider']['group'] = "user"; // user OR admin
 $module_menu['tabbs']['main']  = "Tabbs";
 $module_menu['tabbs']['icon']  = "list-alt";
 $module_menu['tabbs']['group'] = "user"; // user OR admin
-//$module_menu['tabbs']['li'][0]['title'] = "Tous les tabbs";
-//$module_menu['tabbs']['li'][0]['link']  = "index.php?p=tabbs";
-//$module_menu['tabbs']['li'][1]['title'] = "Tous les onglets";
-//$module_menu['tabbs']['li'][1]['link']  = "index.php?p=tabbs&a=alltabs";
+$module_menu['tabbs']['li'][0]['title'] = "Tous les tabbs";
+$module_menu['tabbs']['li'][0]['link']  = "index.php?p=tabbs";
+$module_menu['tabbs']['li'][1]['title'] = "Tous les onglets";
+$module_menu['tabbs']['li'][1]['link']  = "index.php?p=tabbs&a=alltabs";
 
 // -----------------------
 // Menu TABLE
 // -----------------------
-$module_menu['table']['main']  = "Table";
+$module_menu['table']['main']  = "Tableaux";
 $module_menu['table']['icon']  = "table";
 $module_menu['table']['group'] = "user"; // user OR admin
 
