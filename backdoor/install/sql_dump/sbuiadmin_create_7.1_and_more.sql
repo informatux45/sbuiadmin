159468d470a694f20d7d34f3a5f137920217cc6e
informatux45 <informatux45@users.noreply.github.com>
2024-07-29 16:51:17 +0200

    Update sbuiadmin_create_7.1_and_more.sql

--- backdoor/install/sql_dump/sbuiadmin_create_7.1_and_more.sql
+++ backdoor/install/sql_dump/sbuiadmin_create_7.1_and_more.sql
@@ -60,7 +60,8 @@ CREATE TABLE `<DB_PREFIX>sb_blocked_ip` (
   `count` tinyint(4) UNSIGNED NOT NULL DEFAULT '1',
   `blockedtime` bigint(20) NOT NULL,
   `expirationtime` bigint(20) UNSIGNED NOT NULL DEFAULT '0',
-  `reason` varchar(255) NOT NULL
+  `reason` varchar(255) NOT NULL,
+  `infos` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -951,6 +952,18 @@ INSERT INTO `<DB_PREFIX>sb_users` (`id`, `username`, `password`, `email`, `login
 --
 
 --
+-- Index pour la table `sb_blocked_history`
+--
+ALTER TABLE `<DB_PREFIX>sb_blocked_history`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sb_blocked_ip`
+--
+ALTER TABLE `<DB_PREFIX>sb_blocked_ip`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `sb_blocs`
 --
 ALTER TABLE `<DB_PREFIX>sb_blocs`
@@ -1088,6 +1101,16 @@ ALTER TABLE `<DB_PREFIX>sb_users`
 -- AUTO_INCREMENT for dumped tables
 --
 
+--
+-- AUTO_INCREMENT pour la table `sb_blocked_history`
+--
+ALTER TABLE `<DB_PREFIX>sb_blocked_history`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `sb_blocked_ip`
+--
+ALTER TABLE `<DB_PREFIX>sb_blocked_ip`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `sb_blocs`
 --
