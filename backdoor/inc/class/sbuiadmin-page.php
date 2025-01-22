c887a5e23c05ecaa2aa1d3515313fa68b61e73fb
informatux45 <informatux@e.email>
2025-01-22 18:26:23 +0100

    Update pagination - Class Page

--- backdoor/inc/class/sbuiadmin-page.php
+++ backdoor/inc/class/sbuiadmin-page.php
@@ -29,87 +29,14 @@ class page extends Smarty {
 
 	}
 	
-	public function paginate($datas, $infos = false) {
-		// ----------------------------------------------
-		$url         = $datas['url'] . '&';
-		$total       = $datas['total'];
-		$by_page     = $datas['by_page'];
-		$currentPage = $datas['page'];
-		$data_infos  = [
-			 'start' => (($currentPage+1)*$by_page)-$by_page+1
-			,'end'   => ($currentPage+1)*$by_page
-			,'total' => $total
-		];
-		// ----------------------------------------------
-
-		// ----------------------------------------------
-		$pagination = "";
-		$pagination .= '
-			<style>
-			.pagination {
-				list-style-type: none;
-				padding: 10px 0;
-				display: inline-flex;
-				justify-content: space-between;
-				box-sizing: border-box;
-			}
-			.pagination li {
-				box-sizing: border-box;
-				padding-right: 10px;
-			}
-			.pagination li a {
-				box-sizing: border-box;
-				background-color: #e2e6e6;
-				padding: 8px;
-				text-decoration: none;
-				font-size: 12px;
-				font-weight: bold;
-				color: #616872;
-				border-radius: 4px;
-			}
-			.pagination li a:hover {
-				background-color: #d4dada;
-			}
-			.pagination .next a, .pagination .prev a {
-				text-transform: uppercase;
-				font-size: 12px;
-			}
-			.pagination .currentpage a {
-				background-color: #518acb;
-				color: #fff;
-			}
-			.pagination .currentpage a:hover {
-				background-color: #518acb;
-			}
-			</style>';
-		// ----------------------------------------------
-		if (isset($infos) && $infos == true) {
-			$pagination .= $this->paginateInfos($data_infos);
-			$pagination .= '<br>';
-		}
-		// ----------------------------------------------
-        $pagination .= '<ul class="pagination">';
-			$pagination .= '<li class="page-item ' . (($currentPage < 1) ? "disabled" : "") . '">';
-                $pagination .= '<a href="' . $url . 'page=' . $currentPage - 1 . '" class="page-link">' . SBUIADMIN_GLOBAL_PREV . '</a>';
-            $pagination .= '</li>';
-            for($page = 1; $page <= $by_page; $page++) {
-                $pagination .= '<li class="page-item ' . (($currentPage == $page-1) ? "active" : "") . '">';
-					$pagination .= '<a href="' . $url . 'page=' . $page-1 . '" class="page-link">' . $page . '</a>';
-                $pagination .= '</li>';
-			}
-            $pagination .= '<li class="page-item ' . (($currentPage == $by_page) ? "disabled" : "") . '">';
-                $pagination .= '<a href="' . $url . 'page=' . $currentPage + 1 . '" class="page-link">' . SBUIADMIN_GLOBAL_NEXT . '</a>';
-            $pagination .= '</li>';
-        $pagination .= '</ul>';
-		
-		return $pagination;
+	public function pageStart($page_current, $page_limit) {
+		return ($page_current > 1) ? ($page_current*$page_limit)-$page_limit : 0;
 	}
 	
-	public function paginateInfos($datas) {
-		$paginate_infos  = "";
-		$paginate_infos .= sprintf(SBUIADMIN_GLOBAL_PAGE_INFOS, $datas['start'], $datas['end'], $datas['total']);
-		return $paginate_infos;
+	public function pageCurrent($page) {
+		return (isset($page) && is_numeric($page)) ? intval($page) : 1;
 	}
+
 
 }
 
